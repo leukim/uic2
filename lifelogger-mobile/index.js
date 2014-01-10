61d1d748e65fcc6f650de350b5c41bc472bb9c96
Miquel Puig i Pey <puigim1@niksula.hut.fi>
2014-01-10 12:41:48 +0100

    Mobile - Login page

--- lifelogger-mobile/index.js
+++ lifelogger-mobile/index.js
@@ -1,1 +1,12 @@
+USERNAME = "";
 
+$(function () {
+	$("#login-submit").click(function () {
+		if ($("#login-username").prop('value') === "") {
+			$("#login-error").text("Please enter a username");
+		} else {
+			USERNAME = $("#login-username").prop('value');
+			$.mobile.navigate("#page-content");
+		}
+	});
+});
